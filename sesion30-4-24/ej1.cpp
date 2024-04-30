2172f01d14edfaa68e6ad0c77c50bf4f5b30d7ee
japzuniga <japzuniga@uamv.edu.ni>
2024-04-30 14:17:27 -0600

    clase de 30/4/24

---
+++ sesion30-4-24/ej1.cpp
@@ -0,0 +1,13 @@
+//mostrar las vocales
+#include <iostream>
+using namespace std;
+
+char vocales[5] = {'a', 'e', 'i', 'o', 'u'};
+
+void imprimir() {
+    for (int i = 0; i < 5; i++) { cout << vocales[i] << endl; }
+}
+
+main () {
+    imprimir();
+}
