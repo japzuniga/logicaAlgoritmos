8fe5670bba6c6f915082aa05026adc5f1ccdf3bc
japzuniga <japzuniga@uamv.edu.ni>
2024-05-14 22:39:02 -0600

    modularizacion 14/5

---
+++ clasesMayo/sesion14-5-24/principal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "funciones.cpp"
+using namespace std;
+
+main() {
+    showMessage();
+
+    int num1, num2;
+    cout << "ingresar el primer numero: ";
+    cin >> num1;
+    cout << "ingresar el segundo numero: ";
+    cin >> num2;
+    sumarNums(num1, num2);
+}
